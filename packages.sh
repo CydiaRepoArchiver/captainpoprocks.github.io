45a1d693b431a932d8354bfa58a58beeabefbfd5
captainpoprocks <mystery.jackson@gmail.com>
2017-04-19 23:51:20 -0700

    added cortana ring lockglyph

--- packages.sh
+++ packages.sh
@@ -6,4 +6,6 @@ dpkg-deb -bZgzip /Users/JacksonRoth/Documents/github/captainpoprocks.github.io/p
 
 dpkg-deb -bZgzip /Users/JacksonRoth/Documents/github/captainpoprocks.github.io/projects/Cute\ Dragon\ Loader debs
 
+dpkg-deb -bZgzip /Users/JacksonRoth/Documents/github/captainpoprocks.github.io/projects/Cortana\ Ring\ Lockglyph debs
+
 # dpkg-deb -bZgzip /Users/JacksonRoth/Documents/github/captainpoprocks.github.io/projects/<projectname> debs
