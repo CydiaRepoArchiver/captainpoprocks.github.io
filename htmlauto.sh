bdc952ba0dbafa9969dd7fbc3693b45bacbd5fca
captainpoprocks <mystery.jackson@gmail.com>
2017-04-19 23:17:37 -0700

    condensed into one script

---
+++ htmlauto.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd ./projects/
+ls -1 > /Users/JacksonRoth/Documents/github/captainpoprocks.github.io/activeprojects.txt
+cd /Users/JacksonRoth/Documents/github/captainpoprocks.github.io/
+lam ./activeprojects.txt -s " <br>" > activeprojects4html.txt
+gsed '6r activeprojects4html.txt' < index.html > tempPkgLs.txt
+mv tempPkgLs.txt index.html
+rm activeprojects.txt
+rm activeprojects4html.txt
